599d1819159f6fd9f53455e791300340a643191f
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-01 01:32:32 -0600

    Add tests for DarkSkyService

---
+++ spec/services/dark_sky_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe DarkSkyService do
+  it 'returns forecast for the requested location' do
+    service = DarkSkyService.new
+
+    data = service.forecast(39.704907, -104.927672)
+    expect(data).to have_key(:currently)
+    expect(data).to have_key(:minutely)
+    expect(data).to have_key(:hourly)
+    expect(data).to have_key(:daily)
+    expect(data).to have_key(:flags)
+  end
+
+  it 'excludes unneeded data' do
+    service = DarkSkyService.new('minutely,alerts')
+
+    data = service.forecast(39.704907, -104.927672)
+    expect(data).to have_key(:currently)
+    expect(data).to_not have_key(:minutely)
+    expect(data).to have_key(:hourly)
+    expect(data).to have_key(:daily)
+    expect(data).to_not have_key(:alerts)
+  end
+end
