585b6f001d14bb79c642861aded070214be77d3c
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 09:21:36 -0600

    Expect geocoder to use named args

--- spec/facades/google_geocoder_spec.rb
+++ spec/facades/google_geocoder_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe GoogleGeocoderFacade, type: :facade do
   it 'calculates lat/lng for the requested location' do
-    g = GoogleGeocoderFacade.new('denver', 'co')
+    g = GoogleGeocoderFacade.new(city: 'denver', state: 'co')
 
     expect(g.latitude).to be_within(0.5).of(39.7)
     expect(g.longitude).to be_within(0.5).of(-105.0)
   end
 
   it 'returns lat/lng in an array' do
-    g = GoogleGeocoderFacade.coordinates('denver', 'co')
+    g = GoogleGeocoderFacade.coordinates(city: 'denver', state: 'co')
 
     expect(g[0]).to be_within(0.5).of(39.7)
     expect(g[1]).to be_within(0.5).of(-105.0)
