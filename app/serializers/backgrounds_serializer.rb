e26552c768165d71b6f35428a32867cb7b686ed1
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 16:40:20 -0600

    Update serializer to assume list of background options

--- app/serializers/backgrounds_serializer.rb
+++ app/serializers/backgrounds_serializer.rb
@@ -1,14 +1,4 @@
 class BackgroundsSerializer
   include FastJsonapi::ObjectSerializer
-  attribute :height do |photo|
-    photo.height_o
-  end
-
-  attribute :width do |photo|
-    photo.width_o
-  end
-
-  attribute :url do |photo|
-    photo.url_o
-  end
+  attributes :height, :width, :url
 end
