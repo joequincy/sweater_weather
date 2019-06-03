eb20baffd56f23c039c394128faba758eb51a944
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 16:54:53 -0600

    Improve test resilience

--- spec/services/flickr_spec.rb
+++ spec/services/flickr_spec.rb
@@ -6,9 +6,10 @@ describe FlickrService, type: :service do
     photos = f.photos(39.704907, -104.927672)
 
     expect(photos).to be_a(Array)
-    sample = photos.sample
-    expect(sample).to have_key(:url_o)
-    expect(sample).to have_key(:height_o)
-    expect(sample).to have_key(:width_o)
+    photos.each do |sample|
+      expect(sample).to have_key(:url_o)
+      expect(sample).to have_key(:height_o)
+      expect(sample).to have_key(:width_o)
+    end
   end
 end
