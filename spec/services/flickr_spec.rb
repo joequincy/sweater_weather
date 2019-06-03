ff16cb27f2fd604cc7e20b5f361d83b517f742e1
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 16:49:36 -0600

    Add test for flickr service

---
+++ spec/services/flickr_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe FlickrService, type: :service do
+  it 'returns photos for the requested location' do
+    f = FlickrService.new
+    photos = f.photos(39.704907, -104.927672)
+
+    expect(photos).to be_a(Array)
+    sample = photos.sample
+    expect(sample).to have_key(:url_o)
+    expect(sample).to have_key(:height_o)
+    expect(sample).to have_key(:width_o)
+  end
+end
