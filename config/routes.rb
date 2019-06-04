9b67d9631a10a658b6b177d3ea52141abba1e7d6
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 20:53:44 -0600

    Add route to create session

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get '/backgrounds', to: 'backgrounds#index'
 
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
