4b5dbba16ab82b6eb0b242ae044e9a8ee06cab6f
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-04 10:19:53 -0600

    Add create method

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::FavoritesController < Api::V1::BaseController
   def create
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      city, state = params[:location].split(", ")
+      location = Location.find_or_create_by(city: city, state: state)
+      something = user.favorites << location
+      render status: 200
+    else
+      render status: 401, json: { error: "invalid api key" }
+    end
   end
 end
