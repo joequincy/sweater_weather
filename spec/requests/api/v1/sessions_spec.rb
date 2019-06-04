613dd300e675b3931854952418f9050e3145ecd4
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 21:00:22 -0600

    Add sad path tests

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -19,4 +19,42 @@ describe 'api/v1/sessions' do
     expect(data).to have_key(:api_key)
     expect(data[:api_key].length).to eq(64)
   end
+
+  it 'rejects an invalid login attempt (password)' do
+    User.create!(email: 'whatever@example.com', password: 'password')
+    headers = {
+      "ACCEPT" => "application/json",
+      "CONTENT_TYPE" => "application/json"
+    }
+    post '/api/v1/sessions',
+         params: { email: "whatever@example.com",
+                   password: "wrongword" }.to_json,
+         headers: headers
+
+    expect(response.content_type).to eq('application/json')
+    expect(response).to have_http_status(:unauthorized)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data).to have_key(:error)
+    expect(data[:error].length).to eq("invalid email or password")
+  end
+
+  it 'rejects an invalid login attempt (email)' do
+    User.create!(email: 'whatever@example.com', password: 'password')
+    headers = {
+      "ACCEPT" => "application/json",
+      "CONTENT_TYPE" => "application/json"
+    }
+    post '/api/v1/sessions',
+         params: { email: "whyever@example.com",
+                   password: "password" }.to_json,
+         headers: headers
+
+    expect(response.content_type).to eq('application/json')
+    expect(response).to have_http_status(:unauthorized)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data).to have_key(:error)
+    expect(data[:error].length).to eq("invalid email or password")
+  end
 end
