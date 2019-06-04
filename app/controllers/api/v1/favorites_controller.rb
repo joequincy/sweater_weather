afbabc26dbd64f84ab6391f36558dd42b55fa0fa
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-04 10:38:01 -0600

    Add correct response code, success message

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::FavoritesController < Api::V1::BaseController
       city, state = params[:location].split(", ")
       location = Location.find_or_create_by(city: city, state: state)
       something = user.favorites << location
-      render status: 200
+      render status: 201, json: { success: "#{params[:location]} added as favorite"}
     else
       render status: 401, json: { error: "invalid api key" }
     end
