655f733694ebe13cd98200711b124c8b35aafb4c
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-06-03 20:46:27 -0600

    Add rescue for error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def create
-    user = User.create(user_params)
-    render status: 201, json: UsersSerializer.new(user).to_json
+    begin
+      user = User.create(user_params)
+      render status: 201, json: UsersSerializer.new(user).to_json
+    rescue
+      render status: 409, json: UsersSerializer.exists
+    end
   end
 
   private
